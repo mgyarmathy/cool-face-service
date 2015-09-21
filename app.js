df10c7fd7f8177a6ad3dc2ed1f5da7e2bfbcef64
Michael Gyarmathy <mgyarmathy@Crederas-MacBook-Pro-2.local>
2015-09-21 10:09:03 -0500

    added Procfile

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function(req, res) {
     res.send(cool() + '\n')
 })
 
-app.listen(80);
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
