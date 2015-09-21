3cc1dcb551cb90d7f82f92bd684a13345489140e
Michael Gyarmathy <mgyarmathy@Crederas-MacBook-Pro-2.local>
2015-09-21 09:53:50 -0500

    initial release

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var cool = require('cool-ascii-faces');
+
+app.get('/', function(req, res) {
+    res.send(cool() + '\n')
+})
+
+app.listen(1337);
