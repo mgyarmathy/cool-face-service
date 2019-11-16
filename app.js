723c7d33f07f8a9113c35436f9bf022b63b06c3f
Akash Bhatthal <akashbhatthal@gmail.com>
2019-11-16 15:20:26 -0800

    Address CORS issue

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var cool = require('cool-ascii-faces');
 
 var port = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res) {
     res.send(cool() + '\n')
 })
